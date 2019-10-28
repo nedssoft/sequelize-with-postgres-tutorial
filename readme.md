55a3cd0397ea7cac842d2107d1b1f9781ba558fe
Chinedu Orie <nedsoftdeveloper@gmail.com>
2019-10-28 21:12:24 +0100

    Test for failure

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-[![Build Status](https://travis-ci.org/oriechinedu/sequelize-with-postgres-tutorial.svg?branch=master)](https://travis-ci.org/oriechinedu/sequelize-with-postgres-tutorial) [![Coverage Status](https://coveralls.io/repos/github/oriechinedu/sequelize-with-postgres-tutorial/badge.svg?branch=master)](https://coveralls.io/github/oriechinedu/sequelize-with-postgres-tutorial?branch=master) [![Known Vulnerabilities](https://snyk.io/test/github/oriechinedu/sequelize-with-postgres-tutorial/badge.svg)](https://snyk.io/test/github/oriechinedu/sequelize-with-postgres-tutorial)
+[![Build Status](https://travis-ci.org/nedssoft/sequelize-with-postgres-tutorial.svg?branch=master)](https://travis-ci.org/nedssoft/sequelize-with-postgres-tutorial) [![Coverage Status](https://coveralls.io/repos/github/oriechinedu/sequelize-with-postgres-tutorial/badge.svg?branch=master)](https://coveralls.io/github/oriechinedu/sequelize-with-postgres-tutorial?branch=master) 
 
-# Sequelize, Postgres, Node.js tutorial
+# Sequelize, PostgreSQL, Node.js tutorial
 
 
 # Usage
 
-- `git clone git@github.com:oriechinedu/sequelize-with-postgres-tutorial.git && cd sequelize-with-postgres-tutorial`
+- `git clone git@github.com:nedssoft/sequelize-with-postgres-tutorial.git && cd sequelize-with-postgres-tutorial`
 - `cp .env.example .env`
 - Create two Postgres databases one for test and the other for development and assign the values of the connection strings to `DEV_DATABASE_URL` and `TEST_DATABASE_URL`= respectively.
 - `npm install`
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,11 +3,13 @@ const app = require('../server');
 
 describe('Post Endpoints', () => {
   it('should create a new post', async () => {
-    const res = await request(app).post('/api/posts')
-    .send({
-      userId: 1,
-      title: 'test is cool',
-    });
+    const res = await request(app)
+      .post('/api/posts')
+      .send({
+        userId: 1,
+        title: 'test is cool',
+        content: 'Lorem ipsum',
+      });
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('post');
   });
@@ -27,20 +29,38 @@ describe('Post Endpoints', () => {
   });
 
   it('should update a post', async () => {
-    const res = await request(app).put('/api/posts/1')
-    .send({
-      userId: 1,
-      title: 'updated title',
-    });
+    const res = await request(app)
+      .put('/api/posts/1')
+      .send({
+        userId: 1,
+        title: 'updated title',
+        content: 'Lorem ipsum',
+      });
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('post');
-    expect(res.body.post).toHaveProperty('title','updated title');
+    expect(res.body.post).toHaveProperty('title', 'updated title');
+  });
+
+  it('should return status code 500 if db constraint is violated', async () => {
+    const res = await request(app)
+      .post('/api/posts')
+      .send({
+        title: 'test is cool',
+        content: 'Lorem ipsum',
+      });
+    expect(res.statusCode).toEqual(500);
+    expect(res.body).toHaveProperty('error');
   });
 
   it('should delete a post', async () => {
     const res = await request(app).delete('/api/posts/1');
     expect(res.statusCode).toEqual(204);
   });
-});
 
+  it('should respond with status code 404 if resource is not found', async () => {
+    const postId = 1;
+    const res = await request(app).get(`/api/posts/${postId}`);
+    expect(res.statusCode).toEqual(404);
+  });
+});
